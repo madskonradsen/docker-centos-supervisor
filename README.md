fa6562847c38a61853fbcb5b830aef96817c9fdd
Mads Konradsen <madskonradsen@gmail.com>
2016-10-07 19:25:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# CentOS7 docker-image with supervisor
+[![Circle CI](https://circleci.com/gh/madskonradsen/docker-centos-supervisor.svg?style=svg)](https://circleci.com/gh/madskonradsen/docker-centos-supervisor)
+
+It's built on top of the [centos:centos7](https://registry.hub.docker.com/_/centos/) image which only adds 20MB.
+
+## Features
+
+ - Supervisord loads its services from `/etc/supervisor.d/`. Add configurations there to have them launched, by adding entries to your `Dockerfile`. For example: `ADD myService.conf /etc/supervisord.d/myService.conf`
+ - All scripts inside `/config/init/*.sh` are runned automatically on startup before supervisord
+ - Can be launched with both in detached and interactive mode and runs all commands passed to it
+ - The supervisord.conf is setup to log debug information. This can of course be overridden by supplying your own supervisord.conf-file
---
+++ scripts/config/bootstrap.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Buckle up
+set -e
+set -u
+
+# Default supervisord params
+SUPERVISOR_PARAMS='-c /etc/supervisord.conf'
+
+# Make sure that we have folders for logs etc.
+mkdir -p /data/conf /data/run /data/logs
+chmod 711 /data/conf /data/run /data/logs
+
+# Execute all scripts in init
+if [ "$(ls /config/init/)" ]; then
+  for init in /config/init/*.sh; do
+    . $init
+  done
+fi
+
+# If we have an interactive container
+if test -t 0; then
+  # Run supervisord normally(in background-mode)
+  supervisord $SUPERVISOR_PARAMS
+  
+  # Execute commands passed to container and exit, or run bash
+  if [[ $@ ]]; then 
+    eval $@
+  else 
+    export PS1='[\u@\h : \w]\$ '
+    /bin/bash
+  fi
+
+# If container is detached run superviord in the foreground 
+else
+  # Execute additional params
+  if [[ $@ ]]; then 
+    eval $@
+  fi
+  supervisord -n $SUPERVISOR_PARAMS
+fi
